e5dd1d5dd97662f8b7016afb6596b4825de9a11f
calclabz <calclabz@gmail.com>
2026-03-17 17:15:30 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    const hamburger = document.getElementById("hamburger");
+    navLinks.classList.toggle("open");
+    hamburger.classList.toggle("open");
+}
